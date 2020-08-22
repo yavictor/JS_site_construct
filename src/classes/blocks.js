0551445fe448f759255fb50796d06165af447221
yv <yavictor37@yandex.ru>
2020-08-22 16:25:39 +0300

    refactor index and blocks .js

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,8 +1,7 @@
 import {columns, row} from "../utils";
 
 class Block {
-    constructor(type, value, options) {
-        this.type = type
+    constructor(value, options) {
         this.value = value
         this.options = options
     }
@@ -13,7 +12,7 @@ class Block {
 
 export class TitleBlock extends Block{
     constructor(value, options) {
-        super('title', value, options);
+        super(value, options);
     }
 
     toHTML() {
@@ -24,7 +23,7 @@ export class TitleBlock extends Block{
 
 export class ImageBlock extends Block{
     constructor(value, options) {
-        super('image', value, options);
+        super(value, options);
     }
 
     toHTML() {
@@ -36,7 +35,7 @@ export class ImageBlock extends Block{
 
 export class TextColumnsBlock extends Block{
     constructor(value, options) {
-        super('textColumns', value, options);
+        super(value, options);
     }
 
     toHTML() {
@@ -47,7 +46,7 @@ export class TextColumnsBlock extends Block{
 
 export class TextBlock extends Block{
     constructor(value, options) {
-        super('text', value, options);
+        super(value, options);
     }
 
     toHTML() {
--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import './styles/main.css'
 import {model} from "./model";
-import {templates} from "./templates";
 
 const site = document.querySelector('#site')
 
-model.forEach((el) => {
-    const generate = templates[el.type];
-    if (generate) {
-        const html = generate(el);
-        site.insertAdjacentHTML('beforeend', html);
-    }
-    // if (el.type === 'title') {
-    //     html = templates.title(el)
-    // } else if (el.type === 'text') {
-    //     html = templates.text(el);
-    // } else if (el.type === 'textColumns') {
-    //     html = templates.textColumns(el);
-    // }
-
-
-})
+model.forEach((el) => site.insertAdjacentHTML('beforeend', el.toHTML()));
 
 
 
